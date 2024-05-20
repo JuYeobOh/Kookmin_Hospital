e9a5d443a70e2f67645b436304913be0e8ae84ac
JuYeobOh <to765@kookmin.ac.kr>
2024-05-20 21:56:42 +0900

    약 지급 api

--- hospital/views.py
+++ hospital/views.py
@@ -2,6 +2,8 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
+from django.db import transaction
 from .models import Patient, Prescription, PrescriptionDrug, Drug
 from .serializers import PatientSerializer, PrescriptionSerializer, PrescriptionDrugSerializer, DrugSerializer, PrescriptionDrugFlatSerializer
 
@@ -24,7 +26,17 @@ class PrescriptionViewSet(viewsets.ModelViewSet):
             PrescriptionDrug.objects.create(prescription=prescription, drug=drug, **drug_data)
         return Response({'status': 'drugs added'})
     
-    @action(detail=False, methods=['get'], url_path='check_drug')
+    @action(detail=False, methods=['post'], url_path='give_drug')
+    @transaction.atomic
+    def give_drug(self, request):
+        prescription_drugs = PrescriptionDrug.objects.all()
+        for pd in prescription_drugs:
+            if pd.drug.count < pd.quantity:
+                return Response({'error': f'Not enough {pd.drug.name} in stock'}, status=status.HTTP_400_BAD_REQUEST)
+            pd.drug.count -= pd.quantity
+            pd.drug.save()
+        return Response({'status': 'drugs given'}, status=status.HTTP_200_OK)
+    
     def check_drug(self, request):
         prescription_drugs = PrescriptionDrug.objects.all().select_related('prescription__patient', 'drug').order_by('prescription__patient__name', 'drug__name')
         serializer = PrescriptionDrugFlatSerializer(prescription_drugs, many=True)
